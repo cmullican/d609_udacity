4658aa8cb58c6e66e554a5f172ce8bade6e41679
Cathy Mullican <cmulli18@my.wgu.edu>
2025-04-07 21:32:29 -0700

    Add Customer Landing To Trusted script

---
+++ nd027-Data-Engineering-Data-Lakes-AWS-Exercises-main/customer_landing_to_trusted.py
@@ -0,0 +1,47 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+from awsglue import DynamicFrame
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node Customer Landing
+CustomerLanding_node1743978218276 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://cmulli/customer/landing/"], "recurse": True}, transformation_ctx="CustomerLanding_node1743978218276")
+
+# Script generated for node Share with Research
+SqlQuery4972 = '''
+select * from myDataSource
+where shareWithResearchAsOfDate is not NULL
+and shareWithResearchAsOfDate != 0
+
+'''
+SharewithResearch_node1743978385261 = sparkSqlQuery(glueContext, query = SqlQuery4972, mapping = {"myDataSource":CustomerLanding_node1743978218276}, transformation_ctx = "SharewithResearch_node1743978385261")
+
+# Script generated for node Customer Trusted
+EvaluateDataQuality().process_rows(frame=SharewithResearch_node1743978385261, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1743977000604", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+CustomerTrusted_node1743978617546 = glueContext.getSink(path="s3://cmulli/customer/trusted/", connection_type="s3", updateBehavior="LOG", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="CustomerTrusted_node1743978617546")
+CustomerTrusted_node1743978617546.setCatalogInfo(catalogDatabase="stedi",catalogTableName="customer_trusted")
+CustomerTrusted_node1743978617546.setFormat("json")
+CustomerTrusted_node1743978617546.writeFrame(SharewithResearch_node1743978385261)
+job.commit()
